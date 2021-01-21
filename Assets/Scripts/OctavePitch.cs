28006a2257cb80a52c9d38248f0e73b872dc834f
nakisa <nakisa.donnelly@gmail.com>
2021-01-21 14:42:01 -0700

    Piano man
    Sing us a song you're the piano man
    Sing us a song tonight

---
+++ Assets/Scripts/OctavePitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctavePitch : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("The number of semitones to raise the pitch. An octave is 12 semitones")]
+    int numberOfSemitones;
+
+    [SerializeField]
+    private AudioSource audioSource;
+    
+    void Start()
+    {
+        RaisePitchBySemitone();
+    }
+
+   public void RaisePitchBySemitone()
+    {
+        // general rule of thumb is to multiply frequency by 1.05946^n
+        // where n is the number of semitones
+        float multiply = audioSource.pitch * Mathf.Pow(1.05946f, numberOfSemitones);
+
+        audioSource.pitch = multiply;
+    }
+}
